01a4dc351cef4763b6e6c218d522a33b60fd685c
Lucas Ananias <lucas@verticalparts.com.br>
2019-02-04 16:48:45 -0200

    Tratando erros 403

--- src/pages/profile/profile.ts
+++ src/pages/profile/profile.ts
@@ -28,9 +28,18 @@ export class ProfilePage {
         this.cliente = response;
         this.getImageIfExists();
       },
-      error => {});
-   }
+      error => {
+        if (error.status == 403) {
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
   }
+  else {
+    this.navCtrl.setRoot('HomePage');
+  }
+}
+   
+  
 
   getImageIfExists(){
     this.clienteService.getImageFromBucket(this.cliente.id)
