106ea0e1811704e18568e6423044c95968382a05
Lucas Ananias <lucas@verticalparts.com.br>
2019-02-06 09:34:56 -0200

    Criando página de produtos

---
+++ src/models/produto.dto.ts
@@ -0,0 +1,9 @@
+export interface ProdutoDTO {
+    id : string;
+    vper : string;
+    nome : string;
+    modelo : string;
+    marca : string;
+    descricao : string;
+    imageUrl? : string;
+}
--- src/pages/categorias/categorias.ts
+++ src/pages/categorias/categorias.ts
@@ -25,11 +25,12 @@ export class CategoriasPage {
     .subscribe(response => {
       this.items = response;
     },
-    error =>{
-      console.log(error);
-    });
-
-    
+    error =>{});
   }
 
-}
+    showProdutos(){
+      this.navCtrl.push('ProdutosPage');
+    }
+  }
+
+
