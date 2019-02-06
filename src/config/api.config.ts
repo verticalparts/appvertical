2f8545cb34d5b4a4b23818f48d257712789d1632
Lucas Ananias <lucas@verticalparts.com.br>
2019-02-06 11:11:14 -0200

    Carregando imagens dos produtos

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,5 +1,6 @@
 export const API_CONFIG = {
     baseUrl: "http://localhost:9000",
     bucketBaseUrl: "https://s3-sa-east-1.amazonaws.com/imagens-vp/categorias",
-    bucketImageUrl: "https://s3-sa-east-1.amazonaws.com/imagens-vp/user"
+    bucketImageUrl: "https://s3-sa-east-1.amazonaws.com/imagens-vp/user",
+    bucketProdUrl: "https://s3-sa-east-1.amazonaws.com/imagens-vp/produtos"
 }
--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -2,13 +2,9 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { ProdutoService } from '../../services/domain/produto.service';
 import { ProdutoDTO } from '../../models/produto.dto';
-
-/**
- * Generated class for the ProdutosPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { API_CONFIG } from '../../config/api.config';
+import { CategoriaDTO } from '../../models/categoria.dto';
+import { CategoriaService } from '../../services/domain/categoria.service';
 
 @IonicPage()
 @Component({
@@ -17,17 +13,31 @@ import { ProdutoDTO } from '../../models/produto.dto';
 })
 export class ProdutosPage {
   items: ProdutoDTO[];
+  categorias: CategoriaDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public produtoService: ProdutoService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public produtoService: ProdutoService, public categoriaService: CategoriaService) {
   }
 
-  ionViewDidLoad() {
+  ionViewDidLoad() {  
     let categoria_id = this.navParams.get('categoria_id');
     this.produtoService.findByCategoria(categoria_id)
       .subscribe(response => {
         this.items = response['content'];
+        this.loadImageUrls();
       },
       error => {});
   }
 
+
+
+  loadImageUrls() {
+    for (var i=0; i<this.items.length; i++) {
+      let item = this.items[i];
+      this.produtoService.getSmallImageFromBucket(item.id)
+        .subscribe(response => {
+          item.imageUrl = `${API_CONFIG.bucketProdUrl}/VPER-${item.id}.png`;
+        },
+        error => {});
+    }
+  }  
 }
