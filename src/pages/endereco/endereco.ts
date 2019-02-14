63d93a3c47fa2527837fd350841e5e98f64bc6b5
Lucas Ananias <lucas@verticalparts.com.br>
2019-02-14 17:11:11 -0200

    Buscando endereços do banco de dados

--- src/pages/endereco/endereco.ts
+++ src/pages/endereco/endereco.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { EnderecoDTO } from '../../models/endereco.dto';
+import { StorageService } from '../../services/storage_service';
+import { ClienteService } from '../../services/domain/cliente.service';
 
 @IonicPage()
 @Component({
@@ -11,11 +13,28 @@ export class EnderecoPage {
 
   items: EnderecoDTO[];
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, 
+              public navParams: NavParams,
+              public storage: StorageService,
+              public clienteService: ClienteService) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad EnderecoPage');
+    let localUser = this.storage.getLocalUser();
+   if(localUser && localUser.email){
+     this.clienteService.findByEmail(localUser.email)
+      .subscribe(response => {
+        this.items = response['enderecos'];
+      },
+      error => {
+        if (error.status == 403) {
+          this.navCtrl.setRoot('HomePage');
+        }
+      });
+  }
+  else {
+    this.navCtrl.setRoot('HomePage');
+  }
   }
 
 }
